7041547025100a3f69194f96942076164a4dbfc8
pujyank2006 <pujyank2006@gmail.com>
2025-08-01 23:43:56 +0530

    New

--- Routes/url.js
+++ Routes/url.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const { handleGenerateNewUrl, handleGet} = require("../Controllers/url");
 
+// Added a route
 router.post("/", handleGenerateNewUrl);
 router.get("/:shortId", handleGet);
 
